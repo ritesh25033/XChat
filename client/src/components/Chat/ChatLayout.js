d7cd7d847289daa30e1ab2d7a4215c35f3d200bc
ritesh <ritesh25033@gmail.com>
2025-07-07 09:53:30 +0530

    up

--- client/src/components/Chat/ChatLayout.js
+++ client/src/components/Chat/ChatLayout.js
@@ -1,30 +1,3 @@
-// import React, { useEffect } from 'react';
-// import { useAuth } from '../../context/AuthContext';
-// import Sidebar from './Sidebar';
-// import ChatWindow from './ChatWindow';
-// import SearchModal from './SearchModal';
-// import '../../styles/Chat.css';
-
-// const ChatLayout = () => {
-//   const { loadUserRooms, isSearchOpen } = useAuth();
-
-//   useEffect(() => {
-//     loadUserRooms();
-//   }, []);
-
-//   return (
-//     <div id="chat-layout" className="chat-layout">
-//       <Sidebar />
-//       <ChatWindow />
-//       {isSearchOpen && <SearchModal />}
-//     </div>
-//   );
-// };
-
-// export default ChatLayout;
-
-
-
 import React, { useEffect } from 'react';
 import { useAuth } from '../../context/AuthContext';
 import Sidebar from './Sidebar';
--- client/src/services/firebase.js
+++ client/src/services/firebase.js
@@ -1,16 +1,3 @@
-// import { initializeApp } from 'firebase/app';
-// import { getDatabase } from 'firebase/database';
-
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-// };
-
-// const app = initializeApp(firebaseConfig);
-// export const database = getDatabase(app);
-
 
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
